e4da08e64aba2065477821ee6a78879fe7891e17
Yuliya Dzemidovich <yuliya.dzemidovich@gmail.com>
2020-07-29 03:27:05 +0300

    refactoring: extracted working with excel methods

---
+++ ExcelHelper.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExcelImportApp
+{
+    class ExcelHelper
+    {
+        String name = "Лист1"; //название листа в книге эксель для импорта
+        /* книга эксель лежит в корне проекта, а
+         * исполняемый файл приложения в /bin/Debug/
+         * поэтому для получения доступа к книге эксель
+         * переходим на два уровня вверх (два раза ..\\
+         * два слеша потому, что один выступает в роли экранирующего символа)
+         */
+        String constr = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" +
+                        "..\\..\\datasheet.xlsx" +
+                        ";Extended Properties='Excel 12.0 XML;HDR=YES;';";
+        OleDbConnection connection;
+        OleDbCommand command;
+        OleDbDataAdapter adapter;
+
+        public DataTable getDataFromCell(String cell)
+        {
+            connection = new OleDbConnection(constr);
+            command = new OleDbCommand("Select * From [" + name + "$" + cell + "]", connection);
+            connection.Open();
+            adapter = new OleDbDataAdapter(command);
+            DataTable oneCellData = new DataTable();
+            adapter.Fill(oneCellData);
+            connection.Close();
+            return oneCellData;
+        }
+
+        public DataTable getDataFromSheet()
+        {
+            connection = new OleDbConnection(constr);
+            connection.Open();
+            command = new OleDbCommand("Select * From [" + name + "$]", connection);
+            adapter = new OleDbDataAdapter(command);
+            DataTable data = new DataTable();
+            adapter.Fill(data);
+            connection.Close();
+            return data;
+        }
+    }
+}
--- Form1.cs
+++ Form1.cs
@@ -13,6 +13,8 @@ namespace ExcelImportApp
 {
     public partial class Form1 : Form
     {
+        ExcelHelper excel = new ExcelHelper();
+
         public Form1()
         {
             InitializeComponent();
@@ -20,34 +22,15 @@ namespace ExcelImportApp
 
         private void buttonImportRatesFromExcelSheet_Click(object sender, EventArgs e)
         {
-            String name = "Лист1"; //название листа в книге эксель для импорта
-            /* книга эксель лежит в корне проекта, а
-             * исполняемый файл приложения в /bin/Debug/
-             * поэтому для получения доступа к книге эксель
-             * переходим на два уровня вверх (два раза ..\\
-             * два слеша потому, что один выступает в роли экранирующего символа)
-             */
-            String constr = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" +
-                            "..\\..\\datasheet.xlsx" +
-                            ";Extended Properties='Excel 12.0 XML;HDR=YES;';";
-
-            OleDbConnection connection = new OleDbConnection(constr);
-            OleDbCommand command = new OleDbCommand("Select * From [" + name + "$]", connection);
-            connection.Open();
-
-            OleDbDataAdapter adapter = new OleDbDataAdapter(command);
-            DataTable data = new DataTable();
-            adapter.Fill(data);
+            DataTable data = excel.getDataFromSheet();
             dataGridView1.DataSource = data; // заполняем табличку dataGridView
 
-            // b3:b3 - значение ячейки; b1:b3 - диапазон ячеек
-            OleDbCommand command2 = new OleDbCommand("Select * From [" + name + "$b3:b3]", connection);
-            OleDbDataAdapter adapter2 = new OleDbDataAdapter(command2);
-            DataTable data2 = new DataTable();
-            adapter2.Fill(data2);
-            textBoxUsdRate.Text = data2.Columns[0].ToString();
+            //DataTable data2 = excel.getDataFromCell("b3:b3");
+            //textBoxUsdRate.Text = data2.Columns[0].ToString();
 
-            connection.Close();
+            textBoxUsdRate.Text = excel.getDataFromCell("b3:b3").Columns[0].ToString();
+            textBoxEurRate.Text = excel.getDataFromCell("b4:b4").Columns[0].ToString();
+            textBoxRub100Rate.Text = excel.getDataFromCell("b5:b5").Columns[0].ToString();
         }
     }
 }
